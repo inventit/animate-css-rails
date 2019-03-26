2a12e62e4250daaa98ebe30f27367cbb520363eb
Toshiyuki HINA <thina@yourinventit.com>
2019-03-26 10:17:41 +0900

    initial commit

---
+++ lib/animate/css/rails/version.rb
@@ -0,0 +1,7 @@
+module Animate
+  module Css
+    module Rails
+      VERSION = "0.1.0"
+    end
+  end
+end
---
+++ spec/animate/css/rails_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Animate::Css::Rails do
+  it "has a version number" do
+    expect(Animate::Css::Rails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
